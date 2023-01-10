9efb4bdba4de895482a672b7c263269a5bb218fd
SuperFromND <22881403+SuperFromND@users.noreply.github.com>
2023-01-10 15:35:03 -0600

    Remove unnecessary check for fallback font BPP
    fallback_font will always be 4 bytes per pixel due to requiring
    an alpha channel.

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -211,16 +211,15 @@ void load_fallback_font() {
     
     // set color masks based on SDL endianness
     #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-        int shift = (fallback_font.bytes_per_pixel == 3) ? 8 : 0;
-        rmask = 0xff000000 >> shift;
-        gmask = 0x00ff0000 >> shift;
-        bmask = 0x0000ff00 >> shift;
-        amask = 0x000000ff >> shift;
+        rmask = 0xff000000;
+        gmask = 0x00ff0000;
+        bmask = 0x0000ff00;
+        amask = 0x000000ff;
     #else
         rmask = 0x000000ff;
         gmask = 0x0000ff00;
         bmask = 0x00ff0000;
-        amask = (fallback_font.bytes_per_pixel == 3) ? 0 : 0xff000000;
+        amask = 0xff000000;
     #endif
     
     font = SDL_CreateRGBSurfaceFrom((void*)fallback_font.pixel_data, fallback_font.width, fallback_font.height, fallback_font.bytes_per_pixel*8, fallback_font.bytes_per_pixel*fallback_font.width, rmask, gmask, bmask, amask);
